00bf16ef6be8980d8285722a75c5bfee66ee5025
blackzombie <blackzom@gmail.com>
2025-08-10 21:09:38 +0900

    feat: menu side

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import styles from "./App.module.css";
 import Menu from "./pages/menu/Menu";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Home from "./pages/home/Home";
 import List from "./pages/list/List";
+import Home from "./pages/home/Home";
 
 export default function App() {
   return (
-    <>
+    <div className={styles.appContainer}>
       <header className={styles.header}>
         <div className={styles.title}> My Diary</div>
-        <Menu />
       </header>
-      <div className={styles.content}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/list" component={List} />
-          <Redirect to="/" />
-        </Switch>
+      <div className={styles.mainContent}>
+        <aside className={styles.sidebar}>
+          <Menu />
+        </aside>
+        <main className={styles.content}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/list" component={List} />
+            <Redirect to="/" />
+          </Switch>
+        </main>
       </div>
-    </>
+    </div>
   );
 }
